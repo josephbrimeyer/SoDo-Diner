9888992b7aef2a0ffbb12c72f59bdebf541a5635
Joe Brimeyer <jbrimeyer@gmail.com>
2020-10-13 00:44:35 -0700

    server and authentication updates.

---
+++ server.js
@@ -0,0 +1,84 @@
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
+process.env.API_KEY;
+process.env.DB_PASSWORD;
+
+// Requiring passport as we've configured it
+const passport = require("passport");
+const db = require("./models");
+const session = require("express-session");
+
+//Linkedin Login using node js and passport
+const LinkedInStrategy = require("passport-linkedin-oauth2").Strategy;
+const routes = require("./api-routes.js");
+const config = require("./config");
+
+app.set("view engine", "ejs");
+
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: true,
+    secret: "SECRET",
+  })
+);
+
+passport.serializeUser(function (user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function (obj, cb) {
+  cb(null, obj);
+});
+
+passport.use(
+  new LinkedInStrategy(
+    {
+      clientID: config.linkedinAuth.clientID,
+      clientSecret: config.linkedinAuth.clientSecret,
+      callbackURL: config.linkedinAuth.callbackURL,
+      scope: ["r_emailaddress", "r_liteprofile"],
+    },
+    function (token, tokenSecret, profile, done) {
+      return done(null, profile);
+    }
+  )
+);
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve up static assets (usually on heroku)
+app.use(express.static("client/build"));
+
+// We need to use sessions to keep track of our user's login status
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Define API routes here
+const routes = require("./controllers/memberController.js");
+require("./routes/html-routes.js")(app);
+require("./routes/author-api-routes.js")(app);
+require("./routes/post-api-routes.js")(app);
+
+app.use("/", routes);
+
+// Requiring our routes
+require("./routes/html-routes")(app);
+require("./routes/api-routes")(app);
+
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
